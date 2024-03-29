33ef042eaabab45e7d263d3a6144908e5db95a60
jauntyjay09 <jayakumar2001mhk@gmail.com>
2024-03-29 19:33:44 +0530

    hams

---
+++ Common-Styles/common.js
@@ -0,0 +1,5 @@
+// coommon js
+let hamburger = document.querySelector(".navigation nav");
+function toggleNav(){
+    hamburger.style.display = (hamburger.style.display == 'block') ? 'none' : 'block' ;
+}
